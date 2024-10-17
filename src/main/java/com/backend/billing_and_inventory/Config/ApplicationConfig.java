178f643878aa38edabfbd391a05ddb57980402d2
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-10-17 19:35:25 +0530

    Added production frontend url to cors

--- src/main/java/com/backend/billing_and_inventory/Config/ApplicationConfig.java
+++ src/main/java/com/backend/billing_and_inventory/Config/ApplicationConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class ApplicationConfig {
@@ -21,9 +22,11 @@ public class ApplicationConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4000", "https://billing-and-inventory.vercel.app/"));
+//        configuration.setAllowedOrigins(List.of("*"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
